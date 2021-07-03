80af7cccf6cf682dcb964c74e856ba5328642f63
Ian Cappellani <ian.cappellani@mail.utoronto.ca>
2021-07-03 19:03:50 -0400

    Switch to JS rather than server side generation

---
+++ generator.js
@@ -0,0 +1,15 @@
+const renderEl = document.querySelector("h1")
+
+const getRandomEl = a => a[Math.floor(Math.random() * a.length)]
+
+fetch("_data/words.json")
+    .then(response => response.json())
+    .then(data => {
+        const name = `${getRandomEl(data.firstWord)} ${getRandomEl(data.secondWord)} ${getRandomEl(data.thirdWord)}`
+        const textNode = document.createTextNode(name)
+        renderEl.appendChild(textNode)
+    })
+    .catch(() => {
+        const errorNode = document.createTextNode("Whoops, this is broken.")
+        renderEl.appendChild(errorNode)
+    })
